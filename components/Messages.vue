e88a54164f2d5df4829e3424ca63c5023ecebed7
yokoto <youkotoushi@gmail.com>
2020-01-11 15:29:48 +0900

    Add basic UI

---
+++ components/Messages.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="chats-container">
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+  </div>
+</template>
+
+<script>
+import Message from './Message'
+
+export default {
+  components: {
+    Message
+  }
+}
+</script>
+
+<style>
+.chats-container {
+  padding: 16px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="app-layout">
+    <div class="sidebar">
+      <p>チャンネル一覧</p>
+      <p>#general</p>
+      <p>#random</p>
+      <p>#random</p>
+    </div>
+    <div class="main-content">
+      <nuxt />
+    </div>
   </div>
 </template>
 
@@ -24,32 +32,25 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.app-layout {
+  display: flex;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.sidebar {
+  width: 300px;
+  background: #4A4141;
+  height: 100vh;
+  padding: 20px;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+.sidebar p {
+  color: #DDDDDD;
+  padding-top: 4px;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.main-content {
+  width: 100%;
+  background: #F1F1F1;
+  height: 100vh;
 }
 </style>
