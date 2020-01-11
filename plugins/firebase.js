5701225b970de0d4d519cceebcd62c3b89b1da82
yokoto <youkotoushi@gmail.com>
2020-01-12 08:56:06 +0900

    Setup firebase firestore

---
+++ plugins/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'  // firebase全般の機能を利用するために読み込み
+import 'firebase/auth'  // ログイン機能を使うために読み込み
+import 'firebase/firestore'  // データベース機能を使うために読み込み
+
+// firebaseの準備ができている場合のみに設定する
+if (!firebase.apps) {
+  const config = {
+    apiKey: "AIzaSyAVwQDKJ3tvk1W_HHfh7-naUsQExCTgvL0",
+    authDomain: "vuejs-slack-clone-app.firebaseapp.com",
+    databaseURL: "https://vuejs-slack-clone-app.firebaseio.com",
+    projectId: "vuejs-slack-clone-app",
+    storageBucket: "vuejs-slack-clone-app.appspot.com",
+    messagingSenderId: "370225786412"
+  }
+  firebase.initializeApp(config)  // configによってfirebaseの設定を行う
+}
+
+const db = firebase.firestore()  // データベースを定義
+export {
+  firebase,
+  db
+}
